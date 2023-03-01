aab66688408b70a85d9142420e80218cfcad6a85
zsw <tmp@zsw-MacBook-Pro.local>
2023-03-01 17:15:04 +1300

    adjust waiting time

--- cypress/e2e/cip.cy.js
+++ cypress/e2e/cip.cy.js
@@ -6,7 +6,7 @@ describe('cip spec', ()=> {
       cy.xpath('/html/body/div[1]/div[2]/form/div[2]/div[2]/input').should('exist')
       cy.xpath('/html/body/div[1]/div[2]/form/div[2]/div[2]/input').type('python')
       cy.get('button.btn btn-u-lg[type="submit"]').click()
-      cy.wait(5000)
+      cy.wait(4000)
     })
   })
 
